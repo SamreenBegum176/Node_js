ad78be47f24455a8a96aad0384a83c0a42bb18d9
SamreenBegum176 <119723773+SamreenBegum176@users.noreply.github.com>
2024-01-08 18:05:20 +0530

    Intro to middlewares Task-1.2

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("In the middleware");
-    next(); // Allows the request to continue to the next middleware in line
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="tittle"><br><input type="tel" name="size"><button type="submit">Add Product</button><form>');
+});
+// next(); Allows the request to continue to the next middleware in line
+
+app.use('/product',  (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
-app.use((req, res, next) => {
-    console.log("Tn the another middleware");
-    res.send('<h1>Hello from Express js!</h1>');
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello from Express</h1>');
 });
 
 app.listen(4000);
